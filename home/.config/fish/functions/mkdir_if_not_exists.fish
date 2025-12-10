a6e2372a2561b41e482ffb5f9b94af78c4963ed0
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-10 11:56:29 +0900

    Refine mkdir_if_not_exists messages with tests

--- home/.config/fish/functions/mkdir_if_not_exists.fish
+++ home/.config/fish/functions/mkdir_if_not_exists.fish
@@ -2,22 +2,35 @@ function mkdir_if_not_exists
     argparse v/verbose -- $argv
     or return
 
-    set -l making (path filter -v $argv)
+    set absent (path filter -v $argv)
+    set existed_files (path filter -f $argv)
 
-    if set -q _flag_verbose; or status is-interactive
-        set -l ignoring (path filter $argv)
-        if set -q ignoring[1]
-            echo 'Skipping already existed name(s)': $ignoring
-        end
-
-        if set -q making[1]
-            echo mkdir $making
+    if set -q existed_files[1]
+        if set -q existed_files[2]
+            err The specified names are already used by files: $existed_files
         else
-            echo Nothing to mkdir
+            err The specified name is already used by a file: $existed_files
         end
     end
 
-    if set -q making[1]
-        mkdir $making
+    if set -q _flag_verbose
+        set existed_dirs (path filter -d $argv)
+        if set -q existed_dirs[1]
+            if set -q existed_dirs[2]
+                err Skipping already existed directories: $existed_dirs
+            else
+                err Skipping already existed directory: $existed_dirs
+            end
+        end
+
+        if set -q absent[1]
+            err mkdir -p $absent
+        else
+            err Nothing to mkdir
+        end
+    end
+
+    if set -q absent[1]
+        mkdir -p $absent
     end
 end
