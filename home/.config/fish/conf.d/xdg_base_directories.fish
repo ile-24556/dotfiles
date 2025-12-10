da71aa303153fcc53611bdc828895d6739f0e89d
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-10 10:54:52 +0900

    Follow what fish_indent says

--- home/.config/fish/conf.d/xdg_base_directories.fish
+++ home/.config/fish/conf.d/xdg_base_directories.fish
@@ -1,6 +1,6 @@
 # Prepare XDG base directories
-set -gx XDG_DATA_HOME   $HOME/.local/share
+set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
-set -gx XDG_STATE_HOME  $HOME/.local/state
-set -gx XDG_CACHE_HOME  $HOME/.cache
+set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_CACHE_HOME $HOME/.cache
 mkdir_if_not_exists $XDG_DATA_HOME $XDG_CONFIG_HOME $XDG_STATE_HOME $XDG_CACHE_HOME
