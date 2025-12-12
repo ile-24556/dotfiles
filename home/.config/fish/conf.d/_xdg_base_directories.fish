da8c61d85fd746da2ae08513e87fd88d7611acd9
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-12 14:10:39 +0900

    Fix type in comment

--- home/.config/fish/conf.d/_xdg_base_directories.fish
+++ home/.config/fish/conf.d/_xdg_base_directories.fish
@@ -1,4 +1,4 @@
-# XDG Base Directory Specification's environment variabes and base directories
+# XDG Base Directory Specification's environment variables and base directories
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_STATE_HOME $HOME/.local/state
