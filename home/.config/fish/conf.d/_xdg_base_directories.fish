f4cee80600f39e05bff1590134607fffe63a3a2e
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-12 13:30:00 +0900

    Refine code comments

--- home/.config/fish/conf.d/_xdg_base_directories.fish
+++ home/.config/fish/conf.d/_xdg_base_directories.fish
@@ -1,10 +1,11 @@
-# Prepare XDG base directories
+# XDG Base Directory Specification's environment variabes and base directories
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx XDG_CACHE_HOME $HOME/.cache
 mkdir_if_not_exists $XDG_DATA_HOME $XDG_CONFIG_HOME $XDG_STATE_HOME $XDG_CACHE_HOME
 
+# App-specific environment variables
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
