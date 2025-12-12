bc89ad08d8019f983773e4903419829763ad24c8
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-12 13:30:46 +0900

    Define DOTNET_CLI_HOME to keep home clean

--- home/.config/fish/conf.d/_xdg_base_directories.fish
+++ home/.config/fish/conf.d/_xdg_base_directories.fish
@@ -7,5 +7,6 @@ mkdir_if_not_exists $XDG_DATA_HOME $XDG_CONFIG_HOME $XDG_STATE_HOME $XDG_CACHE_H
 
 # App-specific environment variables
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+set -gx DOTNET_CLI_HOME $XDG_DATA_HOME/dotnet
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
