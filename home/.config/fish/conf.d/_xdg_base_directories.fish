63c7202096894591defbfb455a720dfc262f4a04
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-12 13:31:28 +0900

    Define PYTHON_HISTORY to keep home clean

--- home/.config/fish/conf.d/_xdg_base_directories.fish
+++ home/.config/fish/conf.d/_xdg_base_directories.fish
@@ -9,4 +9,5 @@ mkdir_if_not_exists $XDG_DATA_HOME $XDG_CONFIG_HOME $XDG_STATE_HOME $XDG_CACHE_H
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx DOTNET_CLI_HOME $XDG_DATA_HOME/dotnet
 set -gx GOPATH $XDG_DATA_HOME/go
+set -gx PYTHON_HISTORY $XDG_STATE_HOME/python/history
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
