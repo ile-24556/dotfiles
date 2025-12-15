01fb39f5d97e1b4f300686101060d872a7db9b87
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-15 11:51:41 +0900

    Return error code when test is failed

--- tests/test_mkdir_if_not_exists.fish
+++ tests/test_mkdir_if_not_exists.fish
@@ -15,24 +15,28 @@ set expected (begin; err Skipping already existed directory: tmp/dir1 2>&1; err 
 set out (mkdir_if_not_exists -v tmp/dir1 2>&1 | string collect)
 if test $out != $expected
     err Test failed on directory name collision message content\nExpected: `$expected`\nActual: `$out`
+    return 1
 end
 
 set expected (begin; err Skipping already existed directories: tmp/dir1 tmp/dir2 2>&1; err Nothing to mkdir 2>&1; end | string collect)
 set out (mkdir_if_not_exists -v tmp/dir1 tmp/dir2 2>&1 | string collect)
 if test $out != $expected
     err Test failed on directory names collision message content\nExpected: `$expected`\nActual: `$out`
+    return 3
 end
 
 set expected (err The specified name is already used by a file: tmp/file1 2>&1 | string collect)
 set out (mkdir_if_not_exists tmp/file1 2>&1 | string collect)
 if test $out != $expected
     err Test failed on file name collision message content with a file\nExpected: `$expected`\nActual: `$out`
+    return 3
 end
 
 set expected (err The specified names are already used by files: tmp/file1 tmp/file2 2>&1 | string collect)
 set out (mkdir_if_not_exists tmp/file1 tmp/file2 2>&1 | string collect)
 if test $out != $expected
     err Test failed on file names collision message content with two files\nExpected: `$expected`\nActual: `$out`
+    return 4
 end
 
 rm -rf tmp
