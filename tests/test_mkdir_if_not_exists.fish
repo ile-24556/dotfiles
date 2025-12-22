68794e6176b65e29427e6b7d0d6d6307a8640e91
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2025-12-22 15:00:23 +0900

    Source all fish functions in one line

--- tests/test_mkdir_if_not_exists.fish
+++ tests/test_mkdir_if_not_exists.fish
@@ -2,9 +2,7 @@ rm -rf tmp
 mkdir -p tmp/dir1 tmp/dir2
 touch tmp/file1 tmp/file2
 
-source home/.config/fish/functions/log.fish
-source home/.config/fish/functions/err.fish
-source home/.config/fish/functions/mkdir_if_not_exists.fish
+cat home/.config/fish/functions/*.fish | source
 
 mkdir_if_not_exists tmp/dir3
 if not test -d tmp/dir3
