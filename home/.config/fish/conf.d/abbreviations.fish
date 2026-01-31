343b909e8f46f31b8bbea01e71c4538ae092176f
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-01-31 13:29:33 +0900

    Define batman and bathelp as fish abbreviations

--- home/.config/fish/conf.d/abbreviations.fish
+++ home/.config/fish/conf.d/abbreviations.fish
@@ -1,1 +1,3 @@
+abbr --add bathelp --set-cursor -- '% --help | bat --plain --language=help'
+abbr --add batman --set-cursor -- 'man % | bat --plain --language=man'
 abbr --add timeit --set-cursor -- 'time for i in (seq 1000); %; end'
