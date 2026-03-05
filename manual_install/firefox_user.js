dd41cd18541f3682a997b8f4c74179f157bdc86c
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-05 12:46:36 +0900

    Add Firefox user config file

---
+++ manual_install/firefox_user.js
@@ -0,0 +1,14 @@
+// monospace fonts
+user_pref("font.name.monospace.ja", "JetBrains Mono, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.monospace.x-unicode", "JetBrains Mono, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.monospace.x-western", "JetBrains Mono, IBM Plex Sans JP, Noto Emoji");
+
+// san-serif fonts
+user_pref("font.name.sans-serif.ja", "Open Sans, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.sans-serif.x-unicode", "Open Sans, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.sans-serif.x-western", "Open Sans, IBM Plex Sans JP, Noto Emoji");
+
+// serif fonts
+user_pref("font.name.serif.ja", "Georgia Pro, Noto Serif CJK JP, Noto Emoji");
+user_pref("font.name.serif.x-unicode", "Georgia Pro, Noto Serif CJK JP, Noto Emoji");
+user_pref("font.name.serif.x-western", "Georgia Pro, Noto Serif CJK JP, Noto Emoji");
