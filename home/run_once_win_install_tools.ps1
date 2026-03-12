18116dee5ab7cbd8288ef27acf154c10fbd4b493
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-12 13:47:19 +0900

    Add installing script for Windows

---
+++ home/run_once_win_install_tools.ps1
@@ -0,0 +1,54 @@
+Write-Output 'Installing tools on Windows.'
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -Wait -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
+########################################
+# WinGet apps
+########################################
+
+# winget install --exact --id '7zip.7zip'
+# winget install --exact --id 'AgileBits.1Password'
+# winget install --exact --id 'Canonical.Ubuntu.2404'
+# winget install --exact --id 'Git.Git'
+# winget install --exact --id 'GitHub.cli'
+# winget install --exact --id 'Google.JapaneseIME'
+# winget install --exact --id 'ImageMagick.ImageMagick'
+# winget install --exact --id 'jdx.mise'
+# winget install --exact --id 'Microsoft.PowerShell'
+# winget install --exact --id 'Microsoft.PowerToys'
+# winget install --exact --id 'Microsoft.VisualStudio.2022.BuildTools'
+# winget install --exact --id 'Microsoft.VisualStudioCode'
+# winget install --exact --id 'Microsoft.WindowsTerminal'
+# winget install --exact --id 'Microsoft.WSL'
+# winget install --exact --id 'Obsidian.Obsidian'
+# winget install --exact --id 'Rustlang.Rustup'
+# winget install --exact --id 'twpayne.chezmoi'
+
+########################################
+# Tools written in Rust
+########################################
+
+Write-Output 'Installing cargo binaries'
+cargo binstall --locked -y -- `
+    bat `
+    deno `
+    dprint `
+    oxipng `
+    ripgrep `
+    starship `
+    uv `
+
+########################################
+# pre-commit
+########################################
+
+Set-Location "${HOME}/.local/share/chezmoi/"
+uv tool install pre-commit
+pre-commit install
