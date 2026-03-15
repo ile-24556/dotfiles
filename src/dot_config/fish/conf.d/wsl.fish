0f86611220fe7bfb1adfa80295f172f7f2ab1701
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-15 21:19:33 +0900

    Prefer .exe if called on Windows for some tools

--- src/dot_config/fish/conf.d/wsl.fish
+++ src/dot_config/fish/conf.d/wsl.fish
@@ -6,3 +6,26 @@ string match -q '*-WSL2' (uname -r); or return 0
 
 # Allow GitHub CLI to start the default web browser on Windows from WSL shell
 set -gx GH_BROWSER /mnt/c/Windows/explorer.exe
+
+# Conditional call for some commands
+function bat --description 'Choose bat.exe if called on Windows'
+    if string match -q '/mnt/c/*' $PWD
+        bat.exe $argv
+        return
+    end
+    command bat $argv
+end
+function git --description 'Choose git.exe if called on Windows'
+    if string match -q '/mnt/c/*' $PWD
+        command git.exe $argv
+        return
+    end
+    command git $argv
+end
+function rg --description 'Choose rg.exe if called on Windows'
+    if string match -q '/mnt/c/*' $PWD
+        rg.exe $argv
+        return
+    end
+    command rg $argv
+end
