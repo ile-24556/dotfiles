3ed5b064b21097dbd683d9bb768282713f35840a
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-17 20:24:06 +0900

    fix: Use apt-get with quiet option

--- src/run_once_before_ubuntu_install_tools.sh
+++ src/run_once_before_ubuntu_install_tools.sh
@@ -11,7 +11,7 @@ echo 'Installing tools on Ubuntu.'
 
 set -eu
 
-sudo apt update && sudo apt upgrade -y
+sudo apt-get update -q && sudo apt-get upgrade -qy
 
 ########################################
 # fish shell
@@ -19,13 +19,13 @@ sudo apt update && sudo apt upgrade -y
 
 if ! command -v fish; then
   # export TZ='Asia/Tokyo' if you want to avoid stopping here.
-  sudo apt install -y software-properties-common
-  sudo add-apt-repository -y --ppa ppa:fish-shell/release-4
-  sudo apt update
-  sudo apt install -y fish
+  sudo apt-get install -qy software-properties-common
+  sudo add-apt-repository -qy --ppa ppa:fish-shell/release-4
+  sudo apt-get update
+  sudo apt-get install -qy fish
 fi
 
-sudo apt install -y \
+sudo apt-get install -qy \
   git \
   tree \
 
@@ -40,15 +40,15 @@ fi
 if ! command -v gh; then
   echo 'Installing GitHub CLI ...'
   # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
-  (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+  (type -p wget >/dev/null || (sudo apt-get update -q && sudo apt-get install wget -qy)) \
     && sudo mkdir -p -m 755 /etc/apt/keyrings \
     && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
     && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
     && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
     && sudo mkdir -p -m 755 /etc/apt/sources.list.d \
     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-    && sudo apt update \
-    && sudo apt install gh -y
+    && sudo apt-get update -q \
+    && sudo apt-get install gh -qy
 fi
 
 ########################################
