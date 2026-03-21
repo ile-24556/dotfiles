5395d264ea8ee7517191f52678d5ece85a49ccfa
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-21 15:47:34 +0900

    fix: Remove unnecessary mkdir calls

--- install.sh
+++ install.sh
@@ -29,13 +29,11 @@ install_fish() {
     return
   fi
 
-  sudo mkdir -p -m 755 /etc/apt/keyrings
   key='/etc/apt/keyrings/fish-shell.asc'
   curl -fsS 'http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x88421E703EDC7AF54967DED473C9FCC9E2BB48DA' \
     | sudo tee "${key}" > /dev/null
   sudo chmod a+r "${key}"
 
-  sudo mkdir -p -m 755 /etc/apt/sources.list.d
   sudo tee /etc/apt/sources.list.d/fish-shell.sources << EOF
 Types: deb
 URIs: https://ppa.launchpadcontent.net/fish-shell/release-4/ubuntu
