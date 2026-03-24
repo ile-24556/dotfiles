579f73f83901fe23d38654b22136ea28dac43c86
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-24 14:35:11 +0900

    fix: Improve prepend_path

--- src/dot_config/fish/functions/prepend_path.fish
+++ src/dot_config/fish/functions/prepend_path.fish
@@ -1,8 +1,14 @@
 function prepend_path --description 'Prepend paths to PATH if it is unique'
     set -l new_path
     for p in $argv
-        set -l rp (builtin realpath -s -- $p)
-        if contains $rp $new_path; or contains $rp $PATH; or not test -d $rp
+        if not test -d $p
+            if test -f $p
+                echo '[prepend_path] A file path is specified:' $p >&2
+            end
+            continue
+        end
+        set -l rp (command realpath -s -- $p)
+        if contains $rp $new_path; or contains $rp $PATH
             continue
         end
         set -p new_path $rp
