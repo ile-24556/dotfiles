44bd51e31caea231c4ec9c3e02e61ef8965e33bd
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-25 15:11:23 +0900

    style: Tidy PowerShell profile

--- src/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,16 @@
-# Import-Module posh-git
-# Import-Module oh-my-posh
-# Set-PoshPrompt -Theme agnosterplus
+# Starship prompt
 Invoke-Expression (&starship init powershell)
 
-# 2024-08-10
-$Env:PYTHONUTF8 = 1
-$Env:PYTHONDEFAULTENCODING = 1 # doesn't work
-
+# Enviroment variables
 $Env:BAT_CONFIG_PATH = "$HOME\.config\bat\config"
 $Env:DPRINT_CONFIG_DIR = "$HOME\.config\dprint"
+$Env:PYTHONDEFAULTENCODING = 1 # doesn't work
+$Env:PYTHONUTF8 = 1
 
-# UTF-8で出力するプログラム（wavpack等）の文字化けを防ぐ - 2024-10-14
+# Output encoding
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
 
+# Preferred datetime format
 Set-Variable months -Option Constant -Scope Private -Value ([String[]]@(
     '01月', '02月', '03月', '04月', '05月', '06月',
     '07月', '08月', '09月', '10月', '11月', '12月',
@@ -35,87 +33,32 @@ $culture.DateTimeFormat.MonthGenitiveNames            = $months
 [System.Threading.Thread]::CurrentThread.CurrentCulture   = $culture
 [System.Threading.Thread]::CurrentThread.CurrentUICulture = $culture
 
-# 2026-02-28
-function rgn() {
-    rg -t md $args "$HOME/Documents/notes/"
-}
-# Aliases - 2026-03-03
+# Aliases
+function rgn() { rg -t md $args "$HOME/Documents/notes/" }
 Set-Alias -Name open -Value explorer
 Set-Alias -Name ex -Value explorer
-function bh() {
-    bat --plain --language=help $args
-}
-function bm() {
-    bat --plain --language=man $args
-}
-function ga() {
-    git add $args
-}
-function gb() {
-    git branch -v $args
-}
-function gbd() {
-    git branch -d $args
-}
-function gbD() {
-    git branch -D $args
-}
-Remove-Alias gc -Force
-function gc() {
-    git commit $args
-}
-function gca() {
-    git commit --amend $args
-}
-function gd() {
-    git diff $args
-}
-function gdc() {
-    git diff --cached $args
-}
-Remove-Alias gl -Force
-function gl() {
-    git log --oneline $args
-}
-function glf() {
-    git log --format=fuller --date=iso-strict-local $args
-}
-function glg() {
-    git log --oneline --graph $args
-}
-function gll() {
-    git log --date=iso-strict-local $args
-}
-function glsf() {
-    git ls-files $args
-}
-function gmv() {
-    git mv $args
-}
-function grb() {
-    git rebase $args
-}
-function grm() {
-    git rm $args
-}
-function grs() {
-    git restore $args
-}
-function grst() {
-    git reset $args
-}
-function grv() {
-    git revert $args
-}
-function gs() {
-    git status --short $args
-}
-function gsh() {
-    git show $args
-}
-function gsl() {
-    git status $args
-}
-function gw() {
-    git switch $args
-}
+function bh() { bat --plain --language=help $args }
+function bm() { bat --plain --language=man $args }
+function ga() { git add $args }
+function gb() { git branch -v $args }
+function gbd() { git branch -d $args }
+function gbD() { git branch -D $args }
+function gc() { git commit $args }; Remove-Alias gc -Force
+function gca() { git commit --amend $args }
+function gd() { git diff $args }
+function gdc() { git diff --cached $args }; Remove-Alias gl -Force
+function gl() { git log --oneline $args }
+function glf() { git log --format=fuller --date=iso-strict-local $args }
+function glg() { git log --oneline --graph $args }
+function gll() { git log --date=iso-strict-local $args }
+function glsf() { git ls-files $args }
+function gmv() { git mv $args }
+function grb() { git rebase $args }
+function grm() { git rm $args }
+function grs() { git restore $args }
+function grst() { git reset $args }
+function grv() { git revert $args }
+function gs() { git status --short $args }
+function gsh() { git show $args }
+function gsl() { git status $args }
+function gw() { git switch $args }
