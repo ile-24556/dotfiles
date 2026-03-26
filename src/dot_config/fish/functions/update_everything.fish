f543e1186cedd6017f1d2be8e74593dc1a1d339a
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-26 19:59:31 +0900

    fix: Update Deno and mise with their own commands

--- src/dot_config/fish/functions/update_everything.fish
+++ src/dot_config/fish/functions/update_everything.fish
@@ -1,6 +1,24 @@
 function update_everything
     sudo apt upgrade --update
     rustup update
-    cargo binstall --locked (cargo install --list | rg '\.\d+:' | cut -d ' ' -f 1)
+
+    update_cargo_bins
+
     mise upgrade
+    chezmoi update
+end
+
+function update_cargo_bins
+    set -l bins (cargo install --list | rg '\.\d+:' | cut -d ' ' -f 1)
+    set -l bins_to_reserve deno mise
+    set -l bins_to_update
+    for bin in $bins
+        if contains $bin $bins_to_reserve
+            continue
+        end
+        set -a bins_to_update $bin
+    end
+    cargo binstall --disable-strategies compile -- $bins_to_update
+    deno upgrade
+    mise self-update
 end
