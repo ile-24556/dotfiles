9fecce34650ffc6941f4b8817b67431a6e151b67
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-30 22:39:01 +0900

    fix: Allow C~F drives in try_win_exe.fish

--- src/dot_config/fish/functions/try_win_exe.fish
+++ src/dot_config/fish/functions/try_win_exe.fish
@@ -1,5 +1,5 @@
 function try_win_exe --description 'Use the Windows binary in a Windows directory if possible'
-    string match -q '/mnt/c/*' $PWD
+    string match -qr '^/mnt/[c-f]/' $PWD
     and command $argv[1].exe $argv[2..]
     or command $argv[1] $argv[2..]
 end
