c29dabbaf12b3f267198495ad19a86c42b7934b3
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-03-31 13:14:36 +0900

    fix: Install Ubuntu on WSL via wsl command

--- scripts/optional_tools.ps1
+++ scripts/optional_tools.ps1
@@ -7,16 +7,18 @@ if ( -not $IsWindows ) {
 
 # winget install --exact --id '7zip.7zip'
 # winget install --exact --id 'AgileBits.1Password'
-# winget install --exact --id 'Canonical.Ubuntu.2404'
 # winget install --exact --id 'Google.JapaneseIME'
 # winget install --exact --id 'Microsoft.PowerToys'
 # winget install --exact --id 'Microsoft.VisualStudio.2022.BuildTools'
 # winget install --exact --id 'Microsoft.VisualStudioCode'
 # winget install --exact --id 'Microsoft.VisualStudioCode.CLI'
 # winget install --exact --id 'Microsoft.WindowsTerminal'
-# winget install --exact --id 'Microsoft.WSL'
 # winget install --exact --id 'Obsidian.Obsidian'
 
 cargo binstall --disable-strategies compile -- fd-find oxipng
+
+# Ubuntu on WSL
+# winget install --exact --id 'Microsoft.WSL'
+# wsl --install 'Ubuntu-24.04'
 
 Update-Everything
