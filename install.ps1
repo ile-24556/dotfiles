ile-24556/dotfiles

commit 880d908d2a8a60c45ace75b9d253dc70f695dd4b

fix: `mise install` after `chezmoi apply`
If not, update_everything fails because an updating target is missing.

install.ps1
@@ -90,6 +90,8 @@ if ($env:GITHUB_ACTIONS) {
 # start developping dotfiles
 ########################################
 
+mise install
+
 if (-not (Get-Command pre-commit -ErrorAction SilentlyContinue)) {
     uv tool install pre-commit
 }

install.sh
@@ -101,6 +101,8 @@ chezmoi_init_and_apply() {
 }
 
 start_developping_dotfiles() {
+  mise install
+
   if ! command -v pre-commit; then
     uv tool install pre-commit
   fi

commit 0d7e1c637e3150ca02cdac81bbc3ffc1fcffae20

fix: Update mise before installing tools
If not, it fails to install Python.

Fixes CI failures caused by 880d908.

See:
Python install error, missing `lib` directory · jdx/mise · Discussion #8728
https://github.com/jdx/mise/discussions/8728

install.ps1
@@ -90,6 +90,7 @@ if ($env:GITHUB_ACTIONS) {
 # start developping dotfiles
 ########################################
 
+mise self-update
 mise install
 
 if (-not (Get-Command pre-commit -ErrorAction SilentlyContinue)) {

install.sh
@@ -101,6 +101,7 @@ chezmoi_init_and_apply() {
 }
 
 start_developping_dotfiles() {
+  mise self-update
   mise install
 
   if ! command -v pre-commit; then