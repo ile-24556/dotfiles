93ca8492446aca71041fce9d1dd9fafade252258
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-02 19:45:33 +0900

    fix: Narrow Firefox profile pattern

--- src/run_after_firefox_configs.fish
+++ src/run_after_firefox_configs.fish
@@ -1,3 +1,3 @@
-for prof in $XDG_CONFIG_HOME/mozilla/firefox/*
+for prof in $XDG_CONFIG_HOME/mozilla/firefox/*.default*/
     cp -r $XDG_DATA_HOME/chezmoi/manual_install/firefox/* $prof
 end
--- src/run_after_firefox_configs.ps1
+++ src/run_after_firefox_configs.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'stop'
 Set-StrictMode -Version 3.0
 Set-PSDebug -Trace 1
 
-foreach ($prof in (Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles").FullName) {
+foreach ($prof in (Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles\*.default*\").FullName) {
     Copy-Item -Force -Recurse -Path "$HOME\.local\share\chezmoi\manual_install\firefox\*" -Destination $prof
 }
