b71f8018afde1f035134ac7a4e81e9690acb3600
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-06 11:39:19 +0900

    feat: Change main sans-serif font to Inter Variable

--- manual_install/firefox/user.js
+++ manual_install/firefox/user.js
@@ -7,9 +7,9 @@ user_pref("font.name.monospace.x-unicode", "JetBrains Mono, IBM Plex Sans JP, No
 user_pref("font.name.monospace.x-western", "JetBrains Mono, IBM Plex Sans JP, Noto Emoji");
 
 // san-serif fonts
-user_pref("font.name.sans-serif.ja", "Open Sans, IBM Plex Sans JP, Noto Emoji");
-user_pref("font.name.sans-serif.x-unicode", "Open Sans, IBM Plex Sans JP, Noto Emoji");
-user_pref("font.name.sans-serif.x-western", "Open Sans, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.sans-serif.ja", "Inter Variable, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.sans-serif.x-unicode", "Inter Variable, IBM Plex Sans JP, Noto Emoji");
+user_pref("font.name.sans-serif.x-western", "Inter Variable, IBM Plex Sans JP, Noto Emoji");
 
 // serif fonts
 user_pref("font.name.serif.ja", "Georgia Pro, Noto Serif, Noto Serif CJK JP, Noto Emoji");
