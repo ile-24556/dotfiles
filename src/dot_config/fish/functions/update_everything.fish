0c0118cae5cd7ebb0f5255958a314afddd21cc19
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-11 16:11:25 +0900

    fix: Upgrade Deno with cargo-binstall
    Deno is not released as frequently as mise.

--- src/dot_config/fish/functions/update_everything.fish
+++ src/dot_config/fish/functions/update_everything.fish
@@ -9,8 +9,7 @@ function update_everything
 end
 
 function update_cargo_bins
-    set -l bins (cargo install --list | rg '\.\d+:' | rg -Fv -e deno -e mise | cut -d ' ' -f 1)
+    set -l bins (cargo install --list | rg '\.\d+:$' | rg -Fv mise | cut -d ' ' -f 1)
     cargo binstall --disable-strategies compile -- $bins
-    deno upgrade
     mise self-update
 end
