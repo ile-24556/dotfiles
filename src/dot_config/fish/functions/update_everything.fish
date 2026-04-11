08568e15e5d7648e7c488d708895c8274b73c9a0
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-11 16:07:31 +0900

    refactor: Simplify code for filtering cargo binary

--- src/dot_config/fish/functions/update_everything.fish
+++ src/dot_config/fish/functions/update_everything.fish
@@ -9,16 +9,8 @@ function update_everything
 end
 
 function update_cargo_bins
-    set -l bins (cargo install --list | rg '\.\d+:' | cut -d ' ' -f 1)
-    set -l bins_to_reserve deno mise
-    set -l bins_to_update
-    for bin in $bins
-        if contains $bin $bins_to_reserve
-            continue
-        end
-        set -a bins_to_update $bin
-    end
-    cargo binstall --disable-strategies compile -- $bins_to_update
+    set -l bins (cargo install --list | rg '\.\d+:' | rg -Fv -e deno -e mise | cut -d ' ' -f 1)
+    cargo binstall --disable-strategies compile -- $bins
     deno upgrade
     mise self-update
 end
