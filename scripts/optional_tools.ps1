2444e8fa9075783b23ab72de5378b7d633b4ae19
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-18 10:19:22 +0900

    feat: Add WinGet IDs for installing optional apps

--- scripts/optional_tools.ps1
+++ scripts/optional_tools.ps1
@@ -7,13 +7,19 @@ if ( -not $IsWindows ) {
 
 # winget install --exact --id '7zip.7zip'
 # winget install --exact --id 'AgileBits.1Password'
+# winget install --exact --id 'Amazon.Music'
 # winget install --exact --id 'Google.JapaneseIME'
+# winget install --exact --id 'JTKsoftware.JoyToKey'
 # winget install --exact --id 'Microsoft.PowerToys'
 # winget install --exact --id 'Microsoft.VisualStudio.2022.BuildTools'
 # winget install --exact --id 'Microsoft.VisualStudioCode'
 # winget install --exact --id 'Microsoft.VisualStudioCode.CLI'
 # winget install --exact --id 'Microsoft.WindowsTerminal'
 # winget install --exact --id 'Obsidian.Obsidian'
+# winget install --exact --id 'PeterPawlowski.foobar2000'
+# winget install --exact --id 'Valve.Steam'
+# winget install --exact --id 'ZedIndustries.Zed'
+# winget install --exact --id 'Zen-Team.Zen-Browser'
 
 cargo binstall --disable-strategies compile -- fd-find oxipng
 
