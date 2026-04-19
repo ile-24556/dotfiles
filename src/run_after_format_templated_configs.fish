3977a966b93deac1352f5adddc0a0ffdb1cdebe3
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-20 08:55:27 +0900

    fix: Surely check all templated file format

--- src/run_after_format_templated_configs.fish
+++ src/run_after_format_templated_configs.fish
@@ -4,9 +4,13 @@ cd (mktemp -d)
 
 cp $XDG_DATA_HOME/chezmoi/.dprint.jsonc .
 
-cp $XDG_CONFIG_HOME/firefox_relay_point/user.js firefox_user.js
-cp $XDG_CONFIG_HOME/fontconfig/fonts.conf font_conf.xml
-cp $XDG_CONFIG_HOME/mise/config.toml mise_config.toml
-cp $XDG_CONFIG_HOME/zed/settings.json zed_settings.jsonc
+mkdir target
+cp $XDG_CONFIG_HOME/firefox_relay_point/user.js target/firefox_user.js
+cp $XDG_CONFIG_HOME/fontconfig/fonts.conf target/font_conf.xml
+cp $XDG_CONFIG_HOME/mise/config.toml target/mise_config.toml
+cp $XDG_CONFIG_HOME/zed/settings.json target/zed_settings.jsonc
 
-dprint check
+for file in target/*
+    echo [format_templated_configs.fish] Checking $file ...
+    dprint check $file
+end
