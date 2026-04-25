1bcb11d6a0775e34faa5dba7e773a596d46db21f
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-26 08:17:36 +0900

    Check gsettings' schemas before setting values

--- src/run_after_set_gnome_fonts.fish
+++ src/run_after_set_gnome_fonts.fish
@@ -1,6 +1,9 @@
 if not command -v gsettings
     return
 end
+if not gsettings list-schemas | grep -F 'org.gnome.desktop.interface'
+    return
+end
 
 gsettings set org.gnome.desktop.interface font-name 'ui-sans-serif 14'
 gsettings set org.gnome.desktop.interface document-font-name 'sans-serif 14'
