65c158dda6955ce1f8e0c4ac4756fd20ec8cb842
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-27 09:17:25 +0900

    fix: Make GNOME fonts smaller

--- src/run_after_set_gnome_fonts.fish
+++ src/run_after_set_gnome_fonts.fish
@@ -5,6 +5,6 @@ if not gsettings list-schemas | grep -Fq 'org.gnome.desktop.interface'
     return
 end
 
-gsettings set org.gnome.desktop.interface font-name 'ui-sans-serif 14'
-gsettings set org.gnome.desktop.interface document-font-name 'sans-serif 14'
-gsettings set org.gnome.desktop.interface monospace-font-name 'monospace 16'
+gsettings set org.gnome.desktop.interface font-name 'ui-sans-serif 12'
+gsettings set org.gnome.desktop.interface document-font-name 'sans-serif 12'
+gsettings set org.gnome.desktop.interface monospace-font-name 'monospace 12'
