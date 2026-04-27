5067850e9ef14c687fbd42d24aab88a0f170fe68
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-27 13:05:22 +0900

    feat: Consider Snap-installed Firefox profiles

--- src/dot_config/firefox_relay_point/run_after_install_firefox_configs.py
+++ src/dot_config/firefox_relay_point/run_after_install_firefox_configs.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+from collections.abc import Generator
 from pathlib import Path
 
 
@@ -8,33 +9,38 @@ def main():
     if (cwd := Path.cwd()) != Path.home() / ".config/firefox_relay_point":
         raise RuntimeError("Unexpected working directory:", cwd)
 
-    match sys.platform:
-        case "linux":
-            if system_is_wsl():
-                print("[Firefox configs] You are in WSL: Skip")
-                return
-            profiles_path = Path(os.environ["XDG_CONFIG_HOME"]) / "mozilla/firefox"
-        case "win32":
-            profiles_path = Path(os.environ["APPDATA"]) / r"Mozilla\Firefox\Profiles"
-        case _:
-            raise RuntimeError(f"[Firefox configs] Unexpected platform: {sys.platform}")
-
-    if not profiles_path.exists():
-        print(f"[Firefox configs] Profiles directory not found:\n{profiles_path}")
-        return
-
     root_src_files = [p for p in Path().glob("*") if p.suffix in (".js", ".css")]
     chrome_src_files = list(Path("chrome").glob("*"))
 
-    for profile in profiles_path.glob(
-        "[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9].*/",
-    ):
+    for profile in collect_profile_paths():
         for p in root_src_files:
             p.copy(profile / p.name)
 
         (chrome_dst_dir := profile / "chrome").mkdir(exist_ok=True)
         for p in chrome_src_files:
             p.copy(chrome_dst_dir / p.name)
+
+
+def collect_profile_paths() -> Generator[Path]:
+    profile_name_pattern = "[a-z0-9]" * 8 + ".*/"
+
+    for root in collect_profile_roots():
+        yield from root.glob(profile_name_pattern)
+
+
+def collect_profile_roots() -> Generator[Path]:
+    match sys.platform:
+        case "linux":
+            if system_is_wsl():
+                print("[Firefox configs] You are in WSL: Skip")
+                return
+
+            yield Path(os.environ["XDG_CONFIG_HOME"]) / "mozilla/firefox"
+            yield Path.home() / "snap/firefox/common/.mozilla/firefox"
+        case "win32":
+            yield Path(os.environ["APPDATA"]) / r"Mozilla\Firefox\Profiles"
+        case _:
+            raise RuntimeError(f"[Firefox configs] Unexpected platform: {sys.platform}")
 
 
 def system_is_wsl() -> bool:
