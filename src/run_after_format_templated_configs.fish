35aad51ad9282d1c966be08b325c46b73b8caa65
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-05-01 19:11:00 +0900

    style: Format chezmoi.toml with dprint

--- src/run_after_format_templated_configs.fish
+++ src/run_after_format_templated_configs.fish
@@ -5,6 +5,7 @@ cd (mktemp -d)
 cp $XDG_DATA_HOME/chezmoi/.dprint.jsonc .
 
 mkdir target
+cp $XDG_CONFIG_HOME/chezmoi/chezmoi.toml target/chezmoi.toml
 cp $XDG_CONFIG_HOME/firefox_relay_point/user.js target/firefox_user.js
 cp $XDG_CONFIG_HOME/fontconfig/fonts.conf target/font_conf.xml
 cp $XDG_CONFIG_HOME/mise/config.toml target/mise_config.toml
